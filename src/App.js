6b7ff2be1db8a47722090659057eae049f76cd34
marteoma <marteomasoyyo@gmail.com>
2018-09-15 20:28:49 -0500

    Cambio arabigo a romano

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import romanize from './conversor';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {value : 
+      {
+        arabian: '',
+        roman: ''
+      }
+    }
+  }
+  handleChange = (e) =>{ 
+    this.setState({value: 
+      {
+        arabian: e.target.value,
+        roman: romanize(e.target.value)
+      }
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -10,6 +28,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">HELLO WORLD</h1>
         </header>
+        <div>
+          <input type="number" value={this.state.value.arabian} onChange={this.handleChange}/>
+          <div>{this.state.value.roman}</div>
+        </div>
       </div>
     );
   }
